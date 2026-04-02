06b619870703c3b65217a1ee5184f16054787122
hsycc0316 <hsycc0316@gmail.com>
2026-04-02 15:06:32 +0800

    chore: remove temporary refactoring scripts and update dependency lockfile

--- app/applet/update_topline.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/core/generation/composing/ToplineEngine.ts', 'utf8');
-
-content = content.replace(
-    "if (section.name.includes('Chorus')) {\n            pitchOffset = style.contrast.chorusPitchOffset || 5;\n        } else if (section.name.includes('Solo')) {",
-    "if (section.name.includes('Chorus')) {\n            pitchOffset = style.contrast.chorusPitchOffset || 5;\n            // 🌟 爆款副歌理论：Detonator (引爆器) 机制\n            if (maxPitchBeforeChorus > 0) {\n                const minimumChorusPeak = maxPitchBeforeChorus + 3;\n                if (60 + pitchOffset < minimumChorusPeak - 4) {\n                    pitchOffset = minimumChorusPeak - 60 - 2;\n                }\n            }\n        } else if (section.name.includes('Solo')) {"
-);
-
-fs.writeFileSync('src/core/generation/composing/ToplineEngine.ts', content);
--- app/applet/update_transform.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/core/generation/composing/ToplineEngine.ts', 'utf8');
-
-content = content.replace(
-    "if (isInv || isRet || isAug) {\n                template = this.transformMotif(template, { isInv, isRet, isAug });",
-    "if (isInv || isRet || isAug || isSwitcheroo) {\n                template = this.transformMotif(template, { isInv, isRet, isAug, isSwitcheroo });"
-);
-
-fs.writeFileSync('src/core/generation/composing/ToplineEngine.ts', content);
